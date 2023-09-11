e3eb84dfeaae1473f57102eb484715b2c87f50b4
alexchikov <alex.chikov@outlook.com>
2023-09-11 12:43:56 +0300

    Fixed logging in client, removed threads from server

--- src/client.py
+++ src/client.py
@@ -2,26 +2,39 @@ import socket
 import logging
 import sys
 
+logging.basicConfig(level=logging.INFO, filename="client.log",
+                    filemode='a+', format="%(levelname)s %(asctime)s: %(message)s")
+
+
 class Client(object):
     def __init__(self, host: str, port: int) -> None:
         self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__client.connect((host, port,))
-        
+        logging.info('setted up client')
+        try:
+            self.__client.connect((host, port,))
+            logging.info('connected to the server')
+        except ConnectionError as e:
+            logging.error(e)
+            sys.exit(1)
+
     def _send_message(self, message: str):
         self.__client.send(message.encode())
+        logging.info('send data to server')
         if message == '\exit':
+            logging.info('disconnected from server')
             self.__client.close()
             sys.exit(0)
-        
+
     def _receive_message(self):
+        logging.info('receive data from server')
         return self.__client.recv(1024).decode()
-    
+
     def run(self, data: str):
         self._send_message(data)
         print(self._receive_message())
-    
+
+
 if __name__ == "__main__":
     c = Client(input('Host: '), int(input('Port: ')))
     while True:
         c.run(input('::'))
-        
--- src/server.py
+++ src/server.py
@@ -3,23 +3,25 @@ import logging
 import random
 import sys
 from typing import ByteString
-from threading import Thread
 
-logging.basicConfig(level=logging.INFO, format = "%(levelname)s %(asctime)s: %(message)s", stream=sys.stdout)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout,
+                    format="%(levelname)s %(asctime)s: %(message)s")
+
 
 class Server(object):
     def __init__(self, host: str = 'localhost', port: int = random.randint(6000, 9000)) -> None:
         self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__server.bind((host, port,))
-        logging.info("started server on {} host and {} port".format(host, port))
+        logging.info(
+            "started server on {} host and {} port".format(host, port))
         logging.info("started listen for connections")
         self.__server.listen(1)
         self.accept_connection()
-    
+
     def accept_connection(self):
         self.__client = self.__server.accept()
         logging.info("client {} connected".format(self.__client[1]))
-        
+
     def _receive_message(self) -> ByteString:
         data = self.__client[0].recv(1024)
         logging.info('message received from client')
@@ -27,10 +29,10 @@ class Server(object):
             return data
         else:
             raise ConnectionResetError('Client disconnected')
-    
+
     def _send_message(self, data: str):
         self.__client[0].send(data)
-        
+
     def run(self):
         while True:
             try:
@@ -39,21 +41,8 @@ class Server(object):
             except ConnectionResetError:
                 logging.warning('client disconnected from server')
                 self.accept_connection()
-    
-    def stop(self):
-        while True:
-            command = input()
-            if command == '\exit':
-                logging.info('server stopped')
-                sys.exit(0)
-                
-    def start(self):
-        runner = Thread(target=s.run, )
-        runner.start()
-        stopper = Thread(target=s.stop)
-        stopper.start()
-    
-            
+
+
 if __name__ == "__main__":
     s = Server()
-    s.start()
+    s.run()
